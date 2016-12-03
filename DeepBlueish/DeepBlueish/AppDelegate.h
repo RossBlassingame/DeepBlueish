0d02646fd99b6851424758dafd33dbad3e4f8a9d
Michael <michael.tang-1@colorado.edu>
2016-12-03 09:50:49 -0700

    My code 🙈

---
+++ DeepBlueish/DeepBlueish/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DeepBlueish
+//
+//  Created by Michael Tang on 12/3/16.
+//  Copyright © 2016 Michael Tang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DeepBlueish/DeepBlueish/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DeepBlueish
+//
+//  Created by Michael Tang on 12/3/16.
+//  Copyright © 2016 Michael Tang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
